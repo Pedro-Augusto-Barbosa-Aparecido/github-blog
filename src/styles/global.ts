8c33edc402c21aab9c5b381f91f109f5c4725135
Pedro-Augusto-Barbosa-Aparecido <77130441+Pedro-Augusto-Barbosa-Aparecido@users.noreply.github.com>
2022-12-31 10:46:52 -0300

    chore config global style

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,3 +1,16 @@
 import { createGlobalStyle } from "styled-components";
 
-export const GlobalStyle = createGlobalStyle``;
+export const GlobalStyle = createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+  
+  body, html {
+    font-family: "Nunito", sans-serif;
+    font-size: 16px;
+    
+    background-color: ${(props) => props.theme["gray-900"]};
+  }
+`;
